1c2f1e89601dbc0c8ea8534cd9b84ce197c7ba14
Robespierre <99456899+robtrivi@users.noreply.github.com>
2023-07-19 23:59:21 -0500

    Actualizado

---
+++ TallerPatterns/src/main/java/com/mycompany/tallerpatterns/CentroDeControl.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tallerpatterns;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author robes
+ */
+public class CentroDeControl {
+        
+    private List listas = new ArrayList();
+    
+    public void suscribir(Notificador n){
+        
+    }
+    public void dessuscribir(Notificador n){
+        
+    }
+    public void notificar(Tarea t){
+        
+    }
+}
---
+++ TallerPatterns/src/main/java/com/mycompany/tallerpatterns/Notificacion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tallerpatterns;
+
+/**
+ *
+ * @author robes
+ */
+public abstract class Notificacion implements Notificador{
+    private String notifiacion;
+    private Tarea tarea;
+    
+}
